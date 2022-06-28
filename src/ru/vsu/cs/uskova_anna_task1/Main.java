80f40b17ff148430d5f1006aec8b0d67e7757981
Annskl <a_uskovaa@mail.ru>
2022-06-28 12:52:41 +0300

    Task_1_14

---
+++ src/ru/vsu/cs/uskova_anna_task1/Main.java
@@ -0,0 +1,31 @@
+package ru.vsu.cs.uskova_anna_task1;
+
+public class Main {
+    public static void main(String[] args) {
+        CyclicQueue<Integer> queue = new CyclicQueue<>();
+        for(int i=0; i<10; i++){
+            queue.add(i);
+        }
+        System.out.println(queue);
+
+        for(int i=0; i<5; i++){
+            queue.poll();
+        }
+        System.out.println(queue);
+
+        for(int i=10; i<20; i++){
+            queue.add(i);
+        }
+        System.out.println(queue);
+
+        for(int i=0; i<5; i++){
+            queue.poll();
+        }
+        System.out.println(queue);
+
+        for(int i=20; i<100; i++){
+            queue.add(i);
+        }
+        System.out.println(queue);
+    }
+}
